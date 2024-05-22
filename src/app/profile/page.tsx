523fafac31712da939f5142a23cb07584c0cccdd
olamide olayemi <olamid15@gmail.com>
2024-05-22 03:13:09 +0100

    middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import axios from "axios";
 import Link from "next/link";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [data, setData] = useState("nothing")
     const logout = async () => {
         try {
             await axios.get('/api/users/logout')
@@ -17,14 +19,27 @@ export default function ProfilePage() {
         }
 
     }
+
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+            </Link>}
+            </h2>
             <hr />
             <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Logout
+            </button>
+            <button onClick={getUserDetails} className="bg-green-800 mt-4 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+                Get User Datails
             </button>
         </div>
     )
